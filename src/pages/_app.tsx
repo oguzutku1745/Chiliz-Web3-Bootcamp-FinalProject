2c7cf2164c272e86c567ef4c6ccdce2fdacf6a07
Oğuz Utku Yıldız <oguzutku1745@gmail.com>
2023-11-12 03:50:50 +0300

    Footer removed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
           	<Menubar>
 					<Component {...pageProps} />
           	</Menubar>
-			<Footer />
+			{ /* <Footer /> */}
 		  </ThirdwebProvider>
 			) : null}
 		</>
